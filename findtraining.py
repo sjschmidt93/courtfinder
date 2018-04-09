d6856709d210dadffe6ac9c7b16320b579e323de
Kevin Renner <krenner2@illinois.edu>
2018-04-08 20:42:49 -0500

    Adding training image date

--- findtraining.py
+++ findtraining.py
@@ -1,30 +1,79 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
+import sys
+import os
 
-image = mpimg.imread('champaign_south.jp2')
-height = image.shape[0]
-width = image.shape[1]
-h_count = height // 300
-w_count = width // 300
-plt.ion()
-plt.show()
 
-count = 0
-# Iterate through 300x300 map squares
-for i in range(h_count):
-    for j in range(w_count):
-        h_start = 300 * i
-        h_end = min(300 * (i+1), height)
-        w_start = 300 * j
-        w_end = min(300 * (j+1), width)
-        sub_image = np.copy(image[h_start:h_end,w_start:w_end,:])
-        sub_image[:,:,3] = 255
-        plt.imshow(sub_image)
-        plt.draw()
-        plt.pause(.001)
-        response = input("Does this contain a basketball court? (y/n) ")
-        if response == 'y':
-            plt.imsave('images/training/court-{}'.format(count), sub_image)
-        plt.clf()
-        count += 1
+def scan_image(filename):
+    image = mpimg.imread(filename)
+    height = image.shape[0]
+    width = image.shape[1]
+    h_count = height // 300
+    w_count = width // 300
+    total = h_count * w_count
+    print("There are {} images".format(h_count * w_count))
+    plt.ion()
+    plt.show()
+
+    choices = np.zeros((h_count, w_count))
+    sub_images = []
+    # Iterate through 300x300 map squares
+    i = 0
+    while i < h_count:
+        j = 0
+        while j < w_count:
+            h_start = 300 * i
+            h_end = min(300 * (i+1), height)
+            w_start = 300 * j
+            w_end = min(300 * (j+1), width)
+            sub_image = np.copy(image[h_start:h_end,w_start:w_end,:])
+            sub_image[:,:,3] = 255
+            sub_images.append(sub_image)
+            plt.imshow(sub_image)
+            plt.draw()
+            plt.pause(.001)
+            response = input("Does this contain a basketball court? (y/n) ")
+            location_name = filename.split('.')[0]
+            if response == 'b':
+                if j > 0:
+                    j -= 1
+                else:
+                    i -= 1
+                    j = w_count - 1
+            if response == 'y':
+                choices[i,j] = 1
+            else:
+                choices[i,j] = 0
+            plt.clf()
+            j += 1
+        i += 1
+    plt.close()
+
+    try:
+        os.makedirs('images/training/yes')
+        os.makedirs('images/training/no')
+        print("Creating Training Directories")
+    except:
+        pass
+
+    choices_flattened = choices.flatten()
+    print("Saving images")
+    for i,item in enumerate(choices_flattened):
+        sub_image = sub_images[i]
+        if item == 1:
+            plt.imsave('images/training/yes/{}-{}'.format(location_name, i), sub_image)
+        else:
+            plt.imsave('images/training/no/{}-{}'.format(location_name, i), sub_image)
+    print("Images Saved")
+
+def main():
+    try:
+        filename = sys.argv[1]
+    except:
+        print("Please enter a filename")
+        return
+    scan_image(filename)
+
+if __name__ == "__main__":
+    main()
