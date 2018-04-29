b387a00f8e57ad2748235a578c1e5a025a3068d7
Kevin Renner <krenner2@illinois.edu>
2018-04-29 00:46:29 -0500

    Adding more to login implementation, integrating with firebase

--- courtfinder/lib/home.dart
+++ courtfinder/lib/home.dart
@@ -1,11 +1,53 @@
 import 'package:courtfinder/api.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'settings.dart';
 import 'court.dart';
 import 'login.dart';
 import 'loc.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new HomeScreenState();
+  }
+  
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  String userToken;
+  String userEmail;
+  String userName;
+
+  void loadUserInfo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      userToken = prefs.get("firebaseToken");
+      userEmail = prefs.get("firebaseEmail");
+      userName = prefs.get("firebaseName");
+      print(userToken);
+    });
+  }
+
+  void logout() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove("firebaseToken");
+    await prefs.remove("firebaseEmail");
+    await prefs.remove("firebaseName");
+    print("Logging out");
+    setState(() {
+      userToken = null;
+      userName = null;
+      userEmail = null;
+    });
+  }
+  
+  @override
+  void initState() {
+    super.initState();
+    loadUserInfo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -24,7 +66,14 @@ class HomeScreen extends StatelessWidget {
             ),
             new Column (
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
+                  new Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      new Text("Hello $userName"),
+                    ],
+                  ),
                   new Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
@@ -57,7 +106,7 @@ class HomeScreen extends StatelessWidget {
                         onPressed: () {
                           Navigator.push(
                             context,
-                            new MaterialPageRoute(builder: (context) => new LoginScreen()),
+                            new MaterialPageRoute(builder: (context) => new LoginScreen(userToken != null)),
                           );
                         },
                         child: new Text('Login'),
@@ -70,7 +119,8 @@ class HomeScreen extends StatelessWidget {
                           );
                         },
                         child: new Text('Settings'),
-                      ),  
+                      ),
+                      new RaisedButton(onPressed: () => logout(), child: new Text("Logout"),)
                     ]
                   )
                 ]
--- courtfinder/lib/login.dart
+++ courtfinder/lib/login.dart
@@ -1,39 +1,56 @@
+import 'package:courtfinder/api.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:async';
 
 // firebase authentication source: https://pub.dartlang.org/packages/firebase_auth
 
-class LoginScreen extends StatelessWidget { 
+class LoginScreen extends StatelessWidget {
+  final bool _tokenExists;
 
-    GoogleSignIn _googleSignIn = new GoogleSignIn(
-      scopes: <String>[
-        'email',
-        'https://www.googleapis.com/auth/contacts.readonly',
-      ],
-    );
+  LoginScreen(this._tokenExists);
 
-    Future<Null> _handleSignIn() async {
-      try {
-        await _googleSignIn.signIn();
-      } catch (error) {
-        print(error);
-      }
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  GoogleSignIn _googleSignIn = new GoogleSignIn(
+    scopes: <String>[
+      'email',
+      'https://www.googleapis.com/auth/contacts.readonly',
+    ],
+  );
+
+  Future<Null> _handleSignIn(context) async {
+    try {
+      GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+      GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      FirebaseUser user = await _auth.signInWithGoogle(
+          idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
+      String userToken = await user.getIdToken();
+      String userEmail = user.email;
+      String userName = user.displayName;
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+
+      await prefs.setString("firebaseToken", userToken);
+      await prefs.setString("firebaseEmail", userEmail);
+      await prefs.setString("firebaseName", userName);
+      Navigator.pop(context);
+    } catch (error) {
+      print(error);
     }
-
-  @override
-  Widget build(BuildContext context) { 
-    
-    final googleSignIn = new GoogleSignIn();  
-
-    return new Scaffold(
-      body: new Center(
-        child: new RaisedButton(
-          onPressed: _handleSignIn,
-          child: new Text('Sign in with Google'),
-        ),  
-      )
-    );
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        body: new Center(
+      child: _tokenExists
+          ? new Text("Already Logged In")
+          : new RaisedButton(
+              onPressed: () => _handleSignIn(context),
+              child: new Text('Sign in with Google'),
+            ),
+    ));
+  }
 }
